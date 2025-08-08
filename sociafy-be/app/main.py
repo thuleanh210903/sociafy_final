5a2fccba6ccdb5d8760981a544b59b93fd333d90
ThuLe <thuanhle219@gmail.com>
2025-08-08 15:31:54 +0700

    feat: middleware client

--- sociafy-be/app/main.py
+++ sociafy-be/app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware 
 from app.api.v1 import routers
 
 app = FastAPI(title="Sociafy API")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 for router, prefix, tag in routers:
     app.include_router(router, prefix=f"/api/v1{prefix}", tags=[tag])
