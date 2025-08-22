f0c6d53a33d3ed17ac0bc6585e9494bdcd3198fb
ThuLe <thuanhle219@gmail.com>
2025-08-22 09:37:00 -0400

    feat: implement delete comment and get all comment by post endpoints

--- sociafy-be/app/api/v1/comment_router.py
+++ sociafy-be/app/api/v1/comment_router.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from fastapi import APIRouter, HTTPException, Request
+from fastapi import APIRouter, HTTPException, Query, Request
 from app.schemas.comment import CommentCreate
 from app.db.supabase_client import supabase
 
@@ -60,3 +60,42 @@ def edit_comment(comment_id: str, comment: CommentCreate, request: Request):
     }).eq("id", comment_id).execute())
 
     return {"message": "Comment updated", "data": res.data}
+
+@router.delete('/delete/{comment_id}')
+def delete_comment(comment_id: str, request: Request):
+    user = request.session.get("user")
+    if not user:
+        raise HTTPException(status_code=401, detail="Not authenticated")
+    
+    # check authorize
+    existing = (
+        supabase.table("comment")
+        .select("id", "user_id")
+        .eq("id", comment_id)
+        .execute()
+    )
+    if not existing.data:
+        raise HTTPException(status_code=404, detail="Comment not found")
+
+    if existing.data[0]["user_id"] != user["id"]:
+        raise HTTPException(status_code=403, detail="Not allowed to delete")
+    
+    #delete
+    supabase.table('comment').delete().eq("id", comment_id).execute()
+
+    return {"message": "Comment deleted"}
+
+
+@router.get('/list')
+def  get_all_comments(post_id: str = Query(...)):
+    # get comment by post id
+    res = supabase.table('comment').select("*").eq("post_id", post_id).order("created_at", desc=False).execute()
+
+    # divide 2 part: root comment and parent comment 
+    comments = []
+    for row in res.data:
+        if not row["parent_comment_id"]:
+            row["replies"] = [r for r in res.data if r["parent_comment_id"] == row["id"]]
+            comments.append(row)
+    
+    return {"comments": comments}
