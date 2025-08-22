36b89bf970e1661994a5d491dc63afe0e79ced73
ThuLe <thuanhle219@gmail.com>
2025-08-22 09:21:16 -0400

    feat: implement update comment endpoint

--- sociafy-be/app/api/v1/comment_router.py
+++ sociafy-be/app/api/v1/comment_router.py
@@ -34,3 +34,29 @@ def add_comment(request: Request, comment: CommentCreate):
     }).execute()
 
     return {"Message": "Comment added", "data": res.data}
+
+@router.put('/edit/{comment_id}')
+def edit_comment(comment_id: str, comment: CommentCreate, request: Request):
+    user = request.session.get("user")
+    if not user:
+        raise HTTPException(status_code=401, detail="Not authenticated")
+    
+    # check authorize
+    existing = (
+        supabase.table("comment")
+        .select("id", "user_id")
+        .eq("id", comment_id)
+        .execute()
+    )
+    if not existing.data:
+        raise HTTPException(status_code=404, detail="Comment not found")
+
+    if existing.data[0]["user_id"] != user["id"]:
+        raise HTTPException(status_code=403, detail="Not allowed to edit")
+    
+    # insert comment
+    res = (supabase.table('comment').update({
+        "content": comment.content,
+    }).eq("id", comment_id).execute())
+
+    return {"message": "Comment updated", "data": res.data}
